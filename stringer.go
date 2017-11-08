7c196f271a6d00090ac457d4f87f2c77167a35b6
Ian Tan <ian.tan@techinasia.com>
2017-11-08 17:19:01 +0800

    Remove useless file

--- stringer.go
+++ stringer.go
@@ -526,6 +526,20 @@ func (g *Generator) buildMap(runs [][]Value, typeName string) {
 	g.Printf(stringMap, typeName)
 }
 
+// buildNameToMsgMap allows the Error() method to retrieve the error message
+// quickly. we do need to generate the slice, though.
+func (g *Generator) buildNameToMsgMap(runs [][]Value, typeName string, runsThreshold int) {
+	// TODO: account for runs. for now we can assume we don't have any.
+	// n tracks where in the 'name' slice we are now
+	var n int
+	for _, values := range runs {
+		for _, value := range values {
+			g.Printf("\t_^s_msg%s[%d:%d]: %s,\n", typeName, "", n, n+len(value.name), &value)
+			n += len(value.name)
+		}
+	}
+}
+
 // Argument to format is the type name.
 const stringMap = `func (i %[1]s) String() string {
 	if str, ok := _%[1]s_map[i]; ok {
